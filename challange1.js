efe69530a1b0122f40c0b46c23420f28966ec67e
bryan-kiplangat <kipbryan.bett@gmail.com>
2023-09-26 07:27:41 +0300

    Code challange: challange1

---
+++ challange1.js
@@ -0,0 +1,42 @@
+// Challange1: Student Grade Generator
+// Instructions: run 'node challange1.js' on your terminal
+// Use the readline module to take user prompts.
+const readline = require('readline').createInterface({
+    // Fun note:
+    // readline.createInterface(input, output, completer)
+    // completer function is used for autocompletion... interesting
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('What is your mark?  ', (marks) => {
+    console.log('Your generated grade is ');
+    generateGrade(marks);
+    readline.close();
+});
+
+
+readline.on('close', ()=> {
+    console.log('Marks to Grade. Bye');
+    process.exit();
+})
+
+
+
+function generateGrade(marks) {
+    if (isNaN(marks) || marks < 0 || marks > 100) { // condition statement(using logical operators)
+        console.log('Please enter a valid grade\n numbered between 0 and 100');
+    } else {
+        if (marks > 79) {
+            console.log("A");
+        } else if (marks >= 60 && marks <= 79) {
+            console.log("B");
+        } else if (marks >= 50 && marks <= 59) {
+            console.log("C");
+        } else if (marks >= 40 && marks <= 49) {
+            console.log("D");
+        } else {
+            console.log("E");
+        }
+    }
+}
